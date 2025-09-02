a5fe79ef29189960ab632e624b7ae16136f0b1c1
LDH <leedoh03@naver.com>
2025-09-03 00:52:05 +0900

    🐛 spec.ts에서 Prisma경로 못읽는 거 수정

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Movie } from './entites/movie.entity';
+import { Injectable } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { PrismaService } from 'src/prisma.service';
