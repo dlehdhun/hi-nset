360386cde71e40bbfac0db983ac731c47d540b95
LDH <leedoh03@naver.com>
2025-09-02 17:48:42 +0900

    ✨ Prisma DB로 전환 (service부분만 적용)

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,38 +2,49 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entites/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { PrismaService } from 'src/prisma.service';
+import { movies } from '@prisma/client';
 
 @Injectable()
 export class MoviesService {
-  private movies: Movie[] = [];
+  constructor(private prismaService: PrismaService) {}
 
-  getAll(): Movie[] {
-    return this.movies;
+  async getAll(): Promise<movies[]> {
+    return this.prismaService.movies.findMany();
   }
 
-  getOne(id: number): Movie {
-    const movie = this.movies.find((movie) => movie.id === id);
-    if (!movie) {
-      throw new NotFoundException(`Movie with ID ${id} not found`);
-    }
-    return movie;
-  }
-
-  deleteOne(id: number) {
-    this.getOne(id);
-    this.movies = this.movies.filter((movie) => movie.id !== id);
-  }
-
-  create(movieData: CreateMovieDto) {
-    this.movies.push({
-      id: this.movies.length + 1,
-      ...movieData,
+  async getOne(id: number): Promise<movies> {
+    return this.prismaService.movies.findUniqueOrThrow({
+      where: {
+        id,
+      },
     });
   }
 
-  update(id: number, updateData: UpdateMovieDto) {
-    const movie = this.getOne(id);
-    this.deleteOne(id);
-    this.movies.push({ ...movie, ...updateData });
+  deleteOne(id: number): Promise<movies> {
+    return this.prismaService.movies.delete({
+      where: { id },
+    });
+  }
+
+  async create(movieData: CreateMovieDto): Promise<movies> {
+    return this.prismaService.movies.create({
+      data: {
+        title: movieData.title,
+        year: movieData.year,
+        genres: movieData.genres,
+      },
+    });
+  }
+
+  async update(id: number, updateData: UpdateMovieDto): Promise<movies> {
+    return this.prismaService.movies.update({
+      where: { id },
+      data: {
+        title: updateData.title,
+        year: updateData.year,
+        genres: updateData.genres,
+      },
+    });
   }
 }
