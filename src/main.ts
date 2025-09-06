e0177792186bd7e68f7acb12d6caa820455f13d5
LDH <leedoh03@naver.com>
2025-09-06 22:46:22 +0900

    ✨ Swagger 설치

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    new ValidationPipe(
-      {
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      })
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('영화 API')
+    .setDescription('영화 API입니다.')
+    .setVersion('1.0')
+    .addTag('movie', '영화 관련')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
